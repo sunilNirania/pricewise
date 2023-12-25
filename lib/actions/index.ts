d5daddd55d8f904e298e8c3d6ca5815aca3aa7c1
sunilNirania <suniln2022@gmail.com>
2023-12-25 12:21:04 +0530

    minor change

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -70,7 +70,7 @@ export async function getAllProducts(){
   try {
     connectToDB();
 
-    const products = await Product.find();
+    const products = await Product.find({});
 
     return products;
   } catch (error: any) {
